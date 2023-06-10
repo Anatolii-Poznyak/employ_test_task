8ba2af847dd8acc0b1ac55ff1a57d9cc7b54578c
Anatolii Pozniak <a13101990a@gmail.com>
2023-06-10 13:58:03 +0300

    Add models of employee + position_model

--- employees_structure/models.py
+++ employees_structure/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Position(models.Model):
+    name = models.CharField(max_length=63)
+
+    def __str__(self):
+        return self.name
+
+
+class Employee(AbstractUser):
+    middle_name = models.CharField(max_length=63)
+    hired = models.DateField()
+    position = models.ForeignKey(
+        to=Position,
+        on_delete=models.SET_NULL,
+        related_name="employees",
+        null=True,
+        blank=True
+    )
+    manager = models.ForeignKey(
+        "self",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return f"{self.last_name} {self.first_name} {self.middle_name}"
