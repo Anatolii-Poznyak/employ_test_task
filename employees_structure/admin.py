9ca7c9c2f0b07e950fee22c0b52670edb6d7cf1d
Anatolii Pozniak <a13101990a@gmail.com>
2023-06-10 15:01:51 +0300

    customize admin panel

--- employees_structure/admin.py
+++ employees_structure/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from employees_structure.models import Position, Employee
+
+admin.site.register(Position)
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = UserAdmin.list_display + (
+        "middle_name",
+        "hired",
+        "position",
+        "manager"
+    )
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Additional info",
+            {"fields": (
+                "middle_name",
+                "hired",
+                "position",
+                "manager"
+            )}
+        ),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            "Additional info",
+            {"fields": (
+                "middle_name",
+                "hired",
+                "position",
+                "manager"
+            )}
+        ),
+    )
+    list_filter = ["position", "manager", "hired"]
+    search_fields = ["first_name", "last_name", "middle_name", "hired", "position__name"]
