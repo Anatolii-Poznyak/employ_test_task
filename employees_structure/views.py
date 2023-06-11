333f991470a7ac98dc6633a5c787b0e34ee9f15a
Anatolii Pozniak <a13101990a@gmail.com>
2023-06-11 23:11:35 +0300

    change pagin

--- employees_structure/views.py
+++ employees_structure/views.py
@@ -20,7 +20,7 @@ def index(request):
 
 class EmployeeListView(LoginRequiredMixin, generic.ListView):
     model = Employee
-    paginate_by = 10
+    paginate_by = 20
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data()
