cb5ba4b58b07cc6a130c3daeb3ccf60cf03a0fcd
Anatolii Pozniak <a13101990a@gmail.com>
2023-06-13 01:37:46 +0300

    Fix models tests ( remove id coz of sqlite)+ replace SetUp

--- tests/test_models.py
+++ tests/test_models.py
@@ -4,63 +4,48 @@ from employees_structure.models import Position, Employee
 
 
 class PositionModelTest(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        Position.objects.create(name="TestPosition")
+    def setUp(self):
+        self.test_position = Position.objects.create(name="TestPosition")
 
     def test_position_label(self):
-        position = Position.objects.get(id=1)
-        field_label = position._meta.get_field("name").verbose_name
+        field_label = self.test_position._meta.get_field("name").verbose_name
         self.assertEquals(field_label, "name")
 
     def test_position_name(self):
-        position = Position.objects.get(id=1)
-        expected_object_name = f"{position.name}"
-        self.assertEquals(expected_object_name, str(position))
+        expected_object_name = f"{self.test_position.name}"
+        self.assertEquals(expected_object_name, str(self.test_position))
 
 
 class EmployeeModelTest(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        test_position = Position.objects.create(name="TestPosition")
-        Employee.objects.create(middle_name="TestMiddle", position=test_position)
-        manager = Employee.objects.create(
-            username="manager", middle_name="TestManager", position=test_position
+    def setUp(self):
+        self.test_position = Position.objects.create(name="TestPosition")
+        self.employee1 = Employee.objects.create(middle_name="TestMiddle", position=self.test_position)
+        self.manager = Employee.objects.create(
+            username="manager", middle_name="TestManager", position=self.test_position
         )
-        Employee.objects.create(
+        self.subordinate1 = Employee.objects.create(
             username="subordinate1",
             middle_name="TestSubordinate1",
-            position=test_position,
-            manager=manager,
+            position=self.test_position,
+            manager=self.manager,
         )
-        Employee.objects.create(
+        self.subordinate2 = Employee.objects.create(
             username="subordinate2",
             middle_name="TestSubordinate2",
-            position=test_position,
-            manager=manager,
+            position=self.test_position,
+            manager=self.manager,
         )
-        Employee.objects.create(
-            username="other", middle_name="TestOther", position=test_position
+        self.other_employee = Employee.objects.create(
+            username="other", middle_name="TestOther", position=self.test_position
         )
 
     def test_employee_middle_name_label(self):
-        employee = Employee.objects.get(id=1)
-        field_label = employee._meta.get_field("middle_name").verbose_name
+        field_label = self.employee1._meta.get_field("middle_name").verbose_name
         self.assertEquals(field_label, "middle name")
 
     def test_employee_position_label(self):
-        employee = Employee.objects.get(id=1)
-        field_label = employee._meta.get_field("position").verbose_name
+        field_label = self.employee1._meta.get_field("position").verbose_name
         self.assertEquals(field_label, "position")
-
-    def test_position_id(self):
-        employee = Employee.objects.get(id=1)
-        position = Position.objects.get(id=1)
-        self.assertEquals(employee.position.id, position.id)
-
-    def test_get_absolute_url(self):
-        employee = Employee.objects.get(id=1)
-        self.assertEquals(employee.get_absolute_url(), "/employees/1/")
 
     def test_get_subordinates(self):
         manager = Employee.objects.get(username="manager")
