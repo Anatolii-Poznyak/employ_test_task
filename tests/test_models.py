194a6b3f475b66b8b440d46d620d4a88fbee53da
Anatolii Pozniak <a13101990a@gmail.com>
2023-06-12 23:38:42 +0300

    add tests Employee Model ( testing labels)

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from employees_structure.models import Position
+from employees_structure.models import Position, Employee
 
 
 class PositionModelTest(TestCase):
@@ -17,3 +17,21 @@ class PositionModelTest(TestCase):
         position = Position.objects.get(id=1)
         expected_object_name = f'{position.name}'
         self.assertEquals(expected_object_name, str(position))
+
+
+class EmployeeModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        test_position = Position.objects.create(name='TestPosition')
+        Employee.objects.create(middle_name='TestMiddle', position=test_position)
+
+    def test_employee_middle_name_label(self):
+        employee = Employee.objects.get(id=1)
+        field_label = employee._meta.get_field('middle_name').verbose_name
+        self.assertEquals(field_label, 'middle name')
+
+    def test_employee_position_label(self):
+        employee = Employee.objects.get(id=1)
+        field_label = employee._meta.get_field('position').verbose_name
+        self.assertEquals(field_label, 'position')
