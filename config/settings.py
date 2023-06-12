02d1ad966aa8f214ba586249b3ba86eeef4cc0ee
Anatolii Pozniak <a13101990a@gmail.com>
2023-06-13 01:50:40 +0300

    switch settings for docker_compose (postgres)

--- config/settings.py
+++ config/settings.py
@@ -99,11 +99,11 @@ STATICFILES_FINDERS = [
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST", "localhost"),
-        "PORT": os.getenv("DB_PORT", "5432"),
+        "NAME": os.environ["POSTGRES_DB"],
+        "USER": os.environ["POSTGRES_USER"],
+        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
+        "HOST": "db",
+        "PORT": os.environ["POSTGRES_PORT"],
     }
 }
 
