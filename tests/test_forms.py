2ed86cfdb7fae315eb4b7aa96aed30adf2197e0e
Anatolii Pozniak <a13101990a@gmail.com>
2023-06-13 01:14:35 +0300

    add more Form Tests (Searchform TransferForm)

--- tests/test_forms.py
+++ tests/test_forms.py
@@ -1,26 +1,82 @@
+from unittest import mock
+
 from django.test import TestCase
 
-from employees_structure.forms import BaseEmployeeForm
-from employees_structure.models import Position
+from employees_structure.forms import (
+    BaseEmployeeForm,
+    EmployeeCreationForm,
+    EmployeeUpdateForm,
+    EmployeeSearchForm,
+    TransferSubordinatesForm,
+)
+from employees_structure.models import Position, Employee
 
 
 class BaseEmployeeFormTest(TestCase):
     def test_valid_data(self):
-        position = Position.objects.create(name='position1')
-        form = BaseEmployeeForm({
-            "username": "test_user",
-            "password1": "abcdef123456",
-            "password2": "abcdef123456",
-            "last_name": "Test",
-            "first_name": "User",
-            "middle_name": "Tester",
-            "hired": "2023-05-31",
-            "email": "testuser@example.com",
-            "position": position.id,
-            "manager": None,
-        })
+        position = Position.objects.create(name="position1")
+        form = BaseEmployeeForm(
+            {
+                "username": "test_user",
+                "password1": "abcdef123456",
+                "password2": "abcdef123456",
+                "last_name": "Test",
+                "first_name": "User",
+                "middle_name": "Tester",
+                "hired": "2023-05-31",
+                "email": "testuser@example.com",
+                "position": position.id,
+                "manager": None,
+            }
+        )
         self.assertTrue(form.is_valid())
         employee = form.save()
         self.assertEqual(employee.username, "test_user")
         self.assertEqual(employee.email, "testuser@example.com")
 
+
+class EmployeeCreationFormTest(TestCase):
+    def test_valid_data(self):
+        position = Position.objects.create(name="position1")
+        form = EmployeeCreationForm(
+            {
+                "username": "test_user",
+                "password1": "abcdef123456",
+                "password2": "abcdef123456",
+                "last_name": "Test",
+                "first_name": "User",
+                "middle_name": "Tester",
+                "hired": "2023-05-31",
+                "email": "testuser@example.com",
+                "position": position.id,
+                "manager": None,
+            }
+        )
+        self.assertTrue(form.is_valid())
+        employee = form.save()
+        self.assertEqual(employee.username, "test_user")
+        self.assertEqual(employee.email, "testuser@example.com")
+
+
+class EmployeeSearchFormTest(TestCase):
+    def test_valid_data(self):
+        form = EmployeeSearchForm(
+            {
+                "employee": "test_user",
+            }
+        )
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.cleaned_data["employee"], "test_user")
+
+
+class TransferSubordinatesFormTest(TestCase):
+    def test_valid_data(self):
+        position = Position.objects.create(name="position1")
+        employee1 = Employee.objects.create(username="employee1", position=position)
+        form = TransferSubordinatesForm(
+            {
+                "new_manager": employee1.id,
+            }
+        )
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.cleaned_data["new_manager"], employee1)
