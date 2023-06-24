7437dfffabd8947b475d914cffb2c54fcadc8c50
Anatolii Pozniak <a13101990a@gmail.com>
2023-06-25 00:09:39 +0300

    fix some typo

--- employees_structure/migrations/0002_custom_migration.py
+++ employees_structure/migrations/0002_custom_migration.py
@@ -1,3 +1,11 @@
+"""
+If we want to work with this code in future - we need to add argument elidable=True to this custom migrations,
+     so 'operations' in 'MIGRATION' class  will be look like:
+     'migrations.RunPython(create_json_with_data, reverse_code=migrations.RunPython.noop, elidable=True)'
+     so it will be not applied in the future, after it was applied at least once. At we will be able to make changes
+     in models, change structure of DB, and superuser + json fake data will not been recreated/reloaded every time
+"""
+
 from django.core.management import call_command
 from django.db import migrations
 
@@ -20,12 +28,6 @@ class Migration(migrations.Migration):
     dependencies = [
         ("employees_structure", "0001_initial"),
     ]
-
-    """If we want to work with this code in future - we need to add argument elidable=True to this custom migrations,
-     so they will be look like: 
-     'migrations.RunPython(create_json_with_data, reverse_code=migrations.RunPython.noop, elidable=True'
-     so it will be not applied in future, after it was applied at least once. At we will be able to make changes
-     in models, change structure of DB, and superuser + json fake data will not been recreated/reloaded every time"""
 
     operations = [
         migrations.RunPython(create_json_with_data),
