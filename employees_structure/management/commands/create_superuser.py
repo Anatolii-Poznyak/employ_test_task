c50ecff47a46175d40204e5e5dd756765f9184a3
Anatolii Pozniak <a13101990a@gmail.com>
2023-06-24 23:24:39 +0300

    change print in create_superuser

--- employees_structure/management/commands/create_superuser.py
+++ employees_structure/management/commands/create_superuser.py
@@ -7,7 +7,7 @@ from employees_structure.models import Employee
 class Command(BaseCommand):
     def handle(self, *args, **options):
         if not Employee.objects.filter(username=os.environ["ADMIN_USERNAME"]).exists():
-            Employee.objects.create_superuser(
+            employee = Employee.objects.create_superuser(
                 username=os.environ["ADMIN_USERNAME"],
                 password=os.environ["ADMIN_PASSWORD"],
                 email=os.environ["ADMIN_EMAIL"],
@@ -15,4 +15,4 @@ class Command(BaseCommand):
                 last_name=os.environ["ADMIN_LAST_NAME"],
                 middle_name=os.environ["ADMIN_MIDDLE_NAME"],
             )
-            print("\033[92mSuperuser {} was created successfully!\033[0m".format(os.environ["ADMIN_USERNAME"]))
+            print(f"\033[92m  Superuser {employee.username} was created successfully!\033[0m")
